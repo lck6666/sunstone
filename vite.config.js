98ec1f07b5d37b5bebbccb076a2d58d2350670c6
lck6666 <1482055312@qq.com>
2025-06-21 12:12:58 +0800

    整合new

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import legacy from '@vitejs/plugin-legacy'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
+import { execSync } from 'child_process';
+
+function runUpdateNewsData() {
+  return {
+    name: 'run-update-news-data',
+    buildStart() {
+      try {
+        execSync('node update-news-data.js');
+        console.log('update-news-data.js 执行成功');
+      } catch (error) {
+        console.error('执行 update-news-data.js 时出错:', error.message);
+      }
+    },
+  };
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"./",
+  base: "./",
   plugins: [
     vue(),
     legacy({
       targets: ['ie>=11'],
       additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
+    }),
+    runUpdateNewsData()
   ],
   resolve: {
     alias: {
@@ -27,4 +44,4 @@ export default defineConfig({
       }
     }
   }
-})
+});
